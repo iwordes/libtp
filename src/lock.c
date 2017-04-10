4f2bdeb0210d79d1d1d6695e738508f58089f418
Ian Wordes <iwordes@student.42.us.org>
2017-04-09 20:08:21 -0700

    Update lock, locked, unlock

--- src/lock.c
+++ src/lock.c
@@ -6,13 +6,13 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 18:30:00 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/09 11:55:30 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/09 20:07:52 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libtp_.h>
 
-bool	tp_lock(t_mutex *lock)
+bool	tp_lock(t_tp_mut *lock)
 {
 	return (pthread_mutex_lock(lock) == 0);
 }
--- src/unlock.c
+++ src/unlock.c
@@ -6,13 +6,13 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 18:28:11 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/09 11:56:07 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/09 20:08:07 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libtp_.h>
 
-void	tp_unlock(t_mutex *lock)
+void	tp_unlock(t_tp_mut *lock)
 {
 	pthread_mutex_unlock(lock);
 }
