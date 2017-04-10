d077f3854e6ddc9dccb20820041f0672621d8ba7
Ian Wordes <iwordes@student.42.us.org>
2017-04-09 19:45:52 -0700

    Update _work_loop

--- src/_work_loop.c
+++ src/_work_loop.c
@@ -6,31 +6,31 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 09:47:10 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/09 17:43:31 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/09 19:39:45 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libtp_.h>
 
-void	done_(void)
+void	done_(t_tp *tp)
 {
-	tp_lock(JOB.lock);
+	tp_lock(&JOB.lock);
 	JOB.cnt -= 1;
 	if (JOB.cnt == 0)
-		tp_evfire(JOB.ev_done);
-	tp_unlock(JOB.lock);
+		tp_evfire(&JOB.ev_done);
+	tp_unlock(&JOB.lock);
 }
 
-void	*tp__work_loop(void *ctx)
+void	*tp__work_loop(t_tp *tp)
 {
 	t_job	*job;
 
 	while (1)
 	{
-		job = tp_qpop();
+		job = tp_qpop(tp);
 		job->fn(job->ctx);
 		free(job);
-		done_();
+		done_(tp);
 	}
 	return (ctx);
 }
