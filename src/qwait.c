d788d01e836d52b58261dc0b4b113d9f9ab06b39
Ian Wordes <iwordes@student.42.us.org>
2017-04-11 10:45:26 -0700

    Add notes

--- src/qwait.c
+++ src/qwait.c
@@ -6,11 +6,24 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 20:20:20 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/09 20:11:28 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/11 10:45:15 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libtp_.h>
+
+/*
+** TODO: Wait for specific tasks
+** (create a per-job condition)
+**
+** tp_qwait(tp, job*, ..., NULL)
+**
+** job
+** {
+**     t_tp_evt	done;
+**     ...
+** }
+*/
 
 void	tp_qwait(t_tp *tp)
 {
